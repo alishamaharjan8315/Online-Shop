76d176e0eebb826019acb8f1f5b6061d520fdf9e
Alisha Maharjan <alishamaharjan147@gmail.com>
2020-02-26 21:16:10 +0545

    Create Db context

---
+++ Shop.Application/Repository/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Application.Repository
+{
+  public class ProductRepository
+  {
+    private ApplicationDbContext _context;
+
+    public ProductRepository(ApplicationDbContext context)
+    {
+      _context = context;
+    }
+
+    public void CreateProduct(int id, string name, string description)
+    {
+      _context.Products.Add(new Product()
+      {
+        Id = id,
+        Name = name,
+        Description = description
+      });
+    }
+  }
+}
